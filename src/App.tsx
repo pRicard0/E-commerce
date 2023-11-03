3ccfc3f2f19557c6af95f635b0a82da9e6ecd324
P. Ricardo <pauloricardo.jesus2011@gmail.com>
2023-11-03 16:22:08 -0300

    tailwind colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      
+     <h1 className="text-base">oi</h1> 
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          'Very-dark-blue': 'hsl(220, 13%, 13%)',
+          'Grayish-blue': 'hsl(220, 14%, 75%)',
+          'Dark-grayish-blue': 'hsl(219, 9%, 45%)',
+          'Light-grayish-blue': 'hsl(223, 64%, 98%)',
+          'White': 'hsl(0, 0%, 100%)',
+          'Black': 'hsl(0, 0%, 0%)'
+        },
+        primary: {
+          'Orange': 'hsl(26, 100%, 55%)',
+          'Pale orange': 'hsl(25, 100%, 94%)'
+        }
+      },
+      fontFamily: {
+        'Kumbh': ['Kumbh Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
