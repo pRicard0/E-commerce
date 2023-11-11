2fe0bed61d9eaa4aa33b9039eee4823bdbcc9ace
P. Ricardo <pauloricardo.jesus2011@gmail.com>
2023-11-10 23:18:51 -0300

    subtraction and addition functions

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,8 +1,29 @@
 import { motion } from 'framer-motion'
 import minus from '../assets/images/icon-minus.svg'
 import plus from '../assets/images/icon-plus.svg'
-import cart from '../assets/images/icon-cart.svg'
 export default function TextArea() {
+    const numberElement = document.getElementById('number')
+    let number = numberElement ? parseInt(numberElement.innerText) || 0 : 0;
+    const subtraction = () => {
+        if(numberElement && number > 0) {
+            --number
+            numberElement.innerText = number.toString()
+            return number
+        } else {
+            return number
+        }
+    }
+    const addition = () => {
+        if(numberElement) {
+            ++number
+            numberElement.innerText = number.toString()
+            return number
+        }
+    }
+    const toAdd = () => {
+        
+    }
+
     return (
         <motion.div initial={{x: '-100vw'}} animate={{x: '0'}} transition={{duration: 0.6, delay: 1}} className="font-Kumbh p-6">
             <div className="space-y-2">
@@ -23,16 +44,18 @@ export default function TextArea() {
 
             <div className='space-y-5'>
                 <div className='flex items-center justify-between bg-neutral-Light-grayish-blue rounded-lg p-3 px-6'>
-                    <button type='button'>
+                    <button id='minus' type='button' onClick={subtraction}>
                         <img src={minus} alt="" />
                     </button>
-                    <p className='font-bold'>0</p>
-                    <button type='button'>
+                    <p id="number" className='font-bold'>0</p>
+                    <button id='plus' type='button' onClick={addition}>
                         <img src={plus} alt="" />
                     </button>
                 </div>
-                <button type="button" className='w-full bg-primary-Orange flex items-center justify-center py-3 rounded-lg'>
-                    <img src={cart} alt=""/>
+                <button 
+                type="button" 
+                className='w-full bg-primary-Orange flex items-center justify-center py-3 rounded-lg'
+                onClick={toAdd}>
                     <p className="text-white font-bold">Add to Cart</p>
                 </button>
             </div>
