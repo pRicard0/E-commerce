a3cbac9c6332376cb7d67b94b1d96565914bdc82
P. Ricardo <pauloricardo.jesus2011@gmail.com>
2024-01-09 22:28:00 -0300

    Picture zoom finished

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -24,15 +24,44 @@ const productImages = [productImage1, productImage2, productImage3, productImage
 
 export default function Product() {
     const [activeThumb, setActiveThumb] = useState(productThumbs[0].id)
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const desktopProductPicture = document.getElementById('desktopProduct')
+
+    const showModal = () => {
+        setIsModalVisible(true)
+    }
+    
+    const hideModal = () => {
+        setIsModalVisible(false)
+    }
 
     const handleClick = (thumb: { id: string, src: string, index: number}) => {
         setActiveThumb(thumb.id)
-
-        const desktopProductPicture = document.getElementById('desktopProduct')
         if (desktopProductPicture instanceof HTMLImageElement) {
             desktopProductPicture.src = productImages[thumb.index]
         }
     }
+
+    const seePreviousPicture = () => {
+        const currentIndex = parseInt(activeThumb.substring(12))
+        console.log(currentIndex)
+
+        if(currentIndex > 1 ) {
+            const previousIndex = currentIndex - 1
+            const previousImageId = `productImage${previousIndex}`
+            setActiveThumb(previousImageId)
+        }
+    }
+
+    const seeNextPicture = () => {
+        const currentIndex = parseInt(activeThumb.substring(12))
+        if(currentIndex < 4) {
+            const nextIndex = currentIndex + 1
+            const nextImageId = `productImage${nextIndex}`
+            setActiveThumb(nextImageId)
+        }
+    }
+
     return (
         <div>
             <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.6, delay: 0.6}} className='relative lg:hidden lg:not-sr-only'>
@@ -47,7 +76,12 @@ export default function Product() {
 
             <div className='hidden not-sr-only lg:block'>
                 <div className='rounded-xl overflow-hidden w-imgThumbWidth'>
-                    <img id='desktopProduct' src={productImage1} alt="" className='w-full lg:block cursor-pointer' />
+                    <img 
+                    id='desktopProduct' 
+                    src={productImage1} 
+                    onClick={() => showModal()}
+                    alt="" 
+                    className='w-full lg:block cursor-pointer' />
                 </div>
                 <div className='flex justify-between pt-9'>
                     {productThumbs.map((thumb) => (
@@ -64,6 +98,51 @@ export default function Product() {
                     ))}
                 </div>
             </div>
+            {isModalVisible && (
+                <div>
+                    <motion.div
+                    initial={{opacity: 0}}
+                    animate={{opacity: 0.80}}
+                    transition={{duration: 0.3}}
+                    id='productModal'
+                    onClick={() => hideModal()}
+                    className='fixed top-0 left-0 bg-black w-full h-screen z-20'>
+                    </motion.div>
+                    <div className='fixed z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                        <div className='relative'>
+                            <img src={productImages[parseInt(activeThumb[12]) - 1]} alt="" width={450} className='rounded-xl'/>
+                            <button aria-label='previous picture'>
+                                <img
+                                onClick={() => seePreviousPicture()} 
+                                src={previous} 
+                                alt="" 
+                                className='bg-white p-4 px-5 rounded-full absolute top-1/2 left-0 transform -translate-x-1/2 -translate-y-12' />
+                            </button>
+                            <button aria-label='next picture'>
+                                <img 
+                                onClick={() => seeNextPicture()}
+                                src={next} 
+                                alt="" 
+                                className='bg-white p-4 px-5 rounded-full absolute top-1/2 right-0 transform translate-x-1/2 -translate-y-12'/>
+                            </button>
+                        </div>
+                        <div className='flex justify-between pt-3'>
+                            {productThumbs.map((thumb) => (
+                                <div
+                                key={thumb.id}
+                                onClick={() => handleClick(thumb)}
+                                className={`${activeThumb === thumb.id ? 'border-2 border-primary-Orange bg-white' : ''} rounded-lg`}>
+                                    <img
+                                    key={thumb.id}
+                                    src={thumb.src}
+                                    width={thumb.width}
+                                    className={`${activeThumb === thumb.id ? 'opacity-40 rounded-md' : 'rounded-lg'} cursor-pointer`}/>
+                                </div>
+                            ))} 
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
